606d05bb2bc33c3c488a59a90d9ea8e7b56c2bab
Anton Pirker <anton@ignaz.at>
2019-06-07 19:12:52 +0200

    Dynamic frequency depending on time range

--- maintainer/core/views.py
+++ maintainer/core/views.py
@@ -8,6 +8,10 @@ from core.models import Metric, Project, Release
 from core.utils import resample
 
 
+MONTH = 30
+YEAR = 365
+
+
 def index(request):
     context = {
         'projects': Project.objects.all().order_by('name'),
@@ -24,6 +28,8 @@ def project_detail(request, slug, zoom=None):
         raise Http404('Project does not exist')
 
     today = timezone.now()
+
+    # Zoom to desired date range
     zoom = zoom or '1M'
     time_deltas = {
         '1M': datetime.timedelta(days=30 * 1),
@@ -35,6 +41,21 @@ def project_detail(request, slug, zoom=None):
     }
     begin = today - time_deltas.get(zoom, datetime.timedelta(days=30 * 1))
 
+    # Decide what frequency to display the data in.
+    days = (today - begin).days
+
+    if 0 < days <= 3 * MONTH:
+        frequency = 'D'
+    elif 3 * MONTH < days <= 1 * YEAR:
+        frequency = 'W'
+    elif 1 * YEAR < days <= 3 * YEAR:
+        frequency = 'M'
+    elif days > 3 * YEAR:
+        frequency = 'Q'
+    else:  # default
+        frequency = 'D'
+
+    # Get the data in the desired frequency
     metrics = Metric.objects.filter(
         project=project,
         date__gte=begin,
@@ -44,17 +65,19 @@ def project_detail(request, slug, zoom=None):
         'metrics__github_bug_issues_now_open',
         'metrics__github_bug_issues_avg_days_open',
     )
+    metrics = resample(metrics, frequency)
 
     releases = Release.objects.filter(
         project=project,
         timestamp__gte=begin,
     )
 
+    # Render the HTML and send to client.
     context = {
         'projects': Project.objects.all().order_by('name'),
         'project': project,
         'zoom': zoom,
-        'metrics': resample(metrics, 'D'),
+        'metrics': metrics,
         'releases': releases,
     }
 
