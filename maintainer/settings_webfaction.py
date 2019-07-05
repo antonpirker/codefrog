b2813e81df4cc83ff6fb2fd158ea6de4954bc544
Anton Pirker <anton@ignaz.at>
2019-07-05 21:46:16 +0200

    Updated settings

--- maintainer/settings_webfaction.py
+++ maintainer/settings_webfaction.py
@@ -1,18 +1,22 @@
 from settings_base import *
 
+import os
+
 ALLOWED_HOSTS = [
     '.codefrog.io',
 ]
 
-STATIC_ROOT = '/home/ignaz/webapps/codefrog_io_static/'
+STATIC_ROOT = '/home/codefrog/webapps/codefrog_io_static/'
 
+DB_PASSWORD = os.environ['CODEFROG_DB_PASSWORD'] \
+    if 'CODEFROG_DB_PASSWORD' in os.environ else 'codefrog'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'codefrog',
         'USER': 'codefrog',
-        'PASSWORD': 'lqxxlXE10QHanrBIAq1y',
+        'PASSWORD': DB_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'CONN_MAX_AGE': 60 * 5,
@@ -21,4 +25,4 @@ DATABASES = {
 
 
 # Maintainer Setup
-GIT_REPO_DIR = '/home/ignaz/webapps/codefrog_io/git_projects'
+GIT_REPO_DIR = '/home/codefrog/webapps/codefrog_io/git_projects'
