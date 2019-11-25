0043be505624b360dc4ba786ecdee5fbcb551955
Anton Pirker <anton@ignaz.at>
2019-11-25 17:10:31 +0100

    Error handling

--- maintainer/settings.py
+++ maintainer/settings.py
@@ -185,9 +185,10 @@ GITHUB_APP_CLIENT_ID = env.str('GITHUB_APP_CLIENT_ID', default=None)
 GITHUB_APP_CLIENT_SECRET = env.str('GITHUB_APP_CLIENT_SECRET', default=None)
 GITHUB_AUTH_REDIRECT_URI = env.str('GITHUB_AUTH_REDIRECT_URI', default=None)
 GITHUB_WEBHOOK_SECRET = env.str('GITHUB_WEBHOOK_SECRET', default=None)
-GITHUB_PRIVATE_KEY = env.str('GITHUB_PRIVATE_KEY', multiline=True, default=None)
-if GITHUB_PRIVATE_KEY:
-    GITHUB_PRIVATE_KEY = GITHUB_PRIVATE_KEY.encode()
+try:
+    GITHUB_PRIVATE_KEY = env.str('GITHUB_PRIVATE_KEY', multiline=True, default=None).encode()
+except Exception:
+    GITHUB_PRIVATE_KEY = None
 GITHUB_WEBHOOK_SECRET = env.str('GITHUB_WEBHOOK_SECRET')
 
 
