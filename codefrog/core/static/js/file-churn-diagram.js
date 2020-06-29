a86a5dfd25da656879223a8652cf3f486e6b54d8
Anton Pirker <anton@ignaz.at>
2020-06-29 11:52:25 +0200

    File churn diagram zoom

--- codefrog/core/static/js/file-churn-diagram.js
+++ codefrog/core/static/js/file-churn-diagram.js
@@ -37,10 +37,52 @@ function createFileChurnDiagram(elementId, labels, values) {
                         precision: 0
                     }
                 }]
+            },
+            plugins: {
+                zoom: {
+                    pan: {
+                        enabled: true,
+                        mode: 'x'
+                    },
+                    zoom: {
+                        enabled: true,
+                        drag: false,
+                        mode: 'x',
+                        sensitivity: 0,
+                        speed: 1,
+                        // Function called once zooming is completed
+                        onZoomComplete: function({chart}) {
+                            let resetButton = document.getElementById('file-churn-diagram-reset-zoom');
+                            resetButton.style.display = 'block';
+                        }
+                    }
+                }
             }
         }
     };
 
     const ctx = document.getElementById(elementId).getContext('2d');
-    return new Chart(ctx, options);
+    let churnChart = new Chart(ctx, options);
+
+    // Setup up reset zoom button
+    let resetButton = document.getElementById(elementId+'-reset-zoom');
+    resetButton && resetButton.addEventListener('click', (element) => {
+        churnChart.resetZoom();
+    });
+
+    // Show a text description of how many files where changed
+    let countOnlyChanges = n => n === 0.1 ? 0 : n;
+    let changes = values.reduce((a, b)=> countOnlyChanges(a) + countOnlyChanges(b), 0);
+
+    let descriptionContext = {
+        changes: changes,
+        filesChanged: values.indexOf(0.1) - 1,
+        filesTotal: labels.length
+    };
+    descriptionContext.filesChangedPercentage = Math.round(descriptionContext.filesChanged / descriptionContext.filesTotal * 100);
+    let descriptionTemplate = `You made ${descriptionContext.changes} changes to ${descriptionContext.filesChanged} files (~${descriptionContext.filesChangedPercentage}%) of a total of ${descriptionContext.filesTotal} files in your codebase over the last 30 days.`;
+    let description = document.getElementById(elementId+'-description');
+    description.innerHTML = descriptionTemplate;
+
+    return churnChart;
 }
