11db6b2ae2b1923bfc7add61e595bb4a1293b323
Anton Pirker <anton@ignaz.at>
2020-09-14 14:44:24 +0200

    Fixed github pull request check.

--- codefrog/connectors/github/handlers.py
+++ codefrog/connectors/github/handlers.py
@@ -12,6 +12,7 @@ from git import Repo
 from connectors.github.utils import create_check_run, get_access_token
 from core.models import Project
 from core.utils import get_path_complexity, GitHub
+from pullrequestbot import checks
 from web.models import UserProfile
 
 logger = structlog.get_logger(__name__)
@@ -68,16 +69,23 @@ def installation__deleted(payload, request=None):
 
 
 def check_suite__requested(payload, request=None):
+    """
+    Performs the pull request bot check.
+
+    :param payload:
+    :param request:
+    :return:
+    """
     event = 'check_suite'
     action = 'requested'
 
+    installation_id = payload['installation']['id']
     repository_full_name = payload['repository']['full_name']
+    repository_github_id = payload['repository']['id']
     commit_sha_before = payload[event]['before']
     commit_sha_after = payload[event]['after']
 
-    installation_access_token = get_access_token(
-        payload['installation']['id'],
-    )
+    installation_access_token = get_access_token(installation_id)
 
     # Tell Github we queued our check
     check_run_payload = {
@@ -95,80 +103,27 @@ def check_suite__requested(payload, request=None):
     }
     out = create_check_run(repository_full_name, installation_access_token, check_run_payload)
 
-    # Get the source code
-    git_url = f'https://x-access-token:{installation_access_token}@github.com/{repository_full_name}.git'
-    repo_dir = os.path.join(settings.PROJECT_SOURCE_CODE_DIR, repository_full_name)
+    # Perform check
+    output = checks.perform_complexity_check(
+        installation_access_token=installation_access_token,
+        repository_full_name=repository_full_name,
+        repository_github_id=repository_github_id,
+        commit_sha_before=commit_sha_before,
+        commit_sha_after=commit_sha_after,
+    )
 
-    if os.path.exists(repo_dir):
-        shutil.rmtree(repo_dir)
-    if not os.path.exists(repo_dir):
-        os.makedirs(repo_dir)
-
-    repo = Repo.clone_from(git_url, repo_dir)
-
-    # Calculate complexity before
-    repo.git.reset('--hard', commit_sha_before)
-    complexity_before = get_path_complexity(repo_dir)
-
-    # Calculate complexity after
-    repo.git.reset('--hard', commit_sha_after)
-    complexity_after = get_path_complexity(repo_dir)
-
-    # Calculate change
-    complexity_change = round((100 / complexity_before) * complexity_after - 100, 1)
-
-    # Tell Github the change complexity and that the check is now completed.
-    sunny = '🌞'  # U+1F31E
-    party_cloudy = '⛅'  # U+26C5
-    cloudy = '☁'  # U+2601
-    stormy = '⛈'  # U+26C8
-    unknown = ''  # nothing :)
-
-    if complexity_change <= 0:
-        icon = sunny
-        summary = f"""You have decreased your complexity of the system by {complexity_change:+.1f}%.
-        Well done! You are on the right tracks to make your project more maintainable!"""
-        conclusion = 'success'
-
-    elif 0 < complexity_change <= 2.5:
-        icon = party_cloudy
-        summary = f"""You have increased your complexity of the system by {complexity_change:+.1f}%.
-        This is OK."""
-        conclusion = 'neutral'
-
-    elif 2.5 < complexity_change <= 5:
-        icon = cloudy
-        summary = f"""You have increased your complexity of the system by {complexity_change:+.1f}%.
-        This is OK if you implement some new features. Just make sure, that you keep an eye on the overall complexity."""
-        conclusion = 'neutral'
-
-    elif complexity_change > 5:
-        icon = stormy
-        summary = f"""You have increased your complexity of the system by {complexity_change:+.1f}%.
-        This is not a good sign. Maybe see if you can refactor your code
-        a little to have less complexity."""
-        conclusion = 'neutral'
-
-    else:
-        icon = unknown
-        summary = f"""I do not know the complexity in your system has changed. Strange thing..."""
-        conclusion = 'neutral'
-
-    title = f'{icon} Complexity: {complexity_change:+.1f}%'
-
-    project = Project.objects.get(external_services__github__repository_id=payload['repository']['id'])
-    details_url = request.build_absolute_uri(reverse('project-detail', kwargs={'slug': project.slug}))
-
+    # Set check to completed and display result
     check_run_payload = {
         'name': 'Complexity',
-        'head_sha': commit_sha_after,
         'status': 'completed',
-        'details_url': details_url,
-        'conclusion': conclusion,
+        'head_sha': commit_sha_after,
+        'details_url': None,
+        'conclusion': output['conclusion'],
         'output': {
-            'title': title,
-            'summary': summary,
+            'title': output['title'],
+            'summary': output['summary'],
         }
     }
     out = create_check_run(repository_full_name, installation_access_token, check_run_payload)
+
     return out
