d6446f55304daff01742da1c172ea75555110c89
Anton Pirker <anton.pirker@sentry.io>
2022-01-28 10:38:38 +0100

    using floats instead of ints.

--- codefrog/web/views.py
+++ codefrog/web/views.py
@@ -48,8 +48,8 @@ def connect_github(request):
     bytes = b"%s%s" % (settings.SECRET_KEY.encode("utf8"), plan.encode("utf8"))
     hash = "hash_%s" % hashlib.sha224(bytes).hexdigest()
 
-    foo = 0
-    blub = 1
+    foo = 0.0
+    blub = 1.0
 
     context = {
         "github_state": secrets.token_urlsafe(50),
